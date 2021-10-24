7de3d5531f80ec08a94e15708b1a3c1181bd9093
plgstalter <pierrelouisgstalter67@gmail.com>
2021-10-24 22:45:17 +0200

    presque connecte :/

--- src/connect.py
+++ src/connect.py
@@ -1,22 +1,21 @@
-#from __future__ import print_statement
 import time
-import swagger_client
-from swagger_client.rest import ApiException
+import StravaPythonClient.swagger_client
+import StravaPythonClient.swagger_client.rest as sr
 from pprint import pprint
 from sys import argv
 
 # Configure OAuth2 access token for authorization: strava_oauth
 if len(argv) == 2:
-    swagger_client.configuration.access_token = str(argv[1])
+    StravaPythonClient.swagger_client.swagger_client.configuration.access_token = str(argv[1])
 else:
-    raise AttributeError('Rentrer la clé API svp')
+    raise AttributeError('Rentrer la clef API svp')
 
 # create an instance of the API class
-api_instance = swagger_client.AthletesApi()
+api_instance = StravaPythonClient.swagger_client.swagger_client.AthletesApi()
 
 try: 
     # Get Authenticated Athlete
     api_response = api_instance.getLoggedInAthlete()
     pprint(api_response)
-except ApiException as e:
+except sr.ApiException as e:
     print("Exception when calling AthletesApi->getLoggedInAthlete: %s\n" % e)
