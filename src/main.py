e249eba07fa8877b97292303cb6cdac4d64c808f
plgstalter <pierrelouisgstalter67@gmail.com>
2021-10-29 19:31:08 +0200

    acces plus propre

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from session import Session
 from sys import argv
 
-if len(argv) < 3:
-    raise AttributeError('pas de client id :(')
+if len(argv) < 2:
+    raise AttributeError('path pour les logins SVP')
 
 
-new_session = Session(argv[1], argv[2])
+new_session = Session(argv[1])
+new_session.get_credentials()
 new_session.grant_access()
--- src/session.py
+++ src/session.py
@@ -3,9 +3,8 @@ from selenium import webdriver
 
 class Session:
 
-    def __init__(self, client_id: int, _file: str):
+    def __init__(self, _file: str):
         self.code = None
-        self.client_id = client_id
         file = open(_file, 'r')
         self.mail = file.readline()[:-1]
         self.pswd = file.readline()[:-1]
@@ -45,5 +44,42 @@ class Session:
         end = url.find('&scope')
 
         self.code = url[(start+ 5):end]
+        return None
+
+    def get_credentials(self):
+        '''
+        get client_secret and client_id
+        '''
+
+        wd = webdriver.Safari()
+        wd.get('https://www.strava.com/settings/api')
+
+        cookies_button = wd.find_element_by_xpath('//*[@id="stravaCookieBanner"]/div/div/button')
+        cookies_button.click()
+
+        wd.find_element_by_id('email').send_keys(self.mail)
+        wd.find_element_by_id('password').send_keys(self.pswd)
+
+        login_button = wd.find_element_by_id('login-button')
+        login_button.click()
+
+        time.sleep(2)
+
+        # secret_button = wd.find_element_by_id('show-secret')
+        # secret_button.click()
+
+        token_button = wd.find_element_by_id('show-token')
+        token_button.click()
+
+        _access_token = wd.find_element_by_xpath('//*[@id="API"]/table/tbody/tr[5]/td[2]/span')
+        # _renew = wd.find_element_by_xpath('//*[@id="API"]/table/tbody/tr[6]/td[2]/span')
+
+        self.client_id = wd.find_element_by_xpath('//*[@id="API"]/table/tbody/tr[3]/td[2]').get_attribute('innerHTML')
+        self.access_token = _access_token.get_attribute('innerHTML')
+        # renew = _renew.get_attribute('innerHTML')
+
+        # self.balise = renew
+
+        wd.quit()
         return None
 
