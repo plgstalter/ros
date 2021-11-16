bef147c6362bcf5670672278d81e22e88cbd324a
plgstalter <pierrelouisgstalter67@gmail.com>
2021-11-16 17:25:19 +0100

    recuperation gpx

--- src/gpx.py
+++ src/gpx.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 import time
+
+import selenium
 #from os import system #useless for now
 from session import Session
 import gpxpy 
@@ -53,4 +55,33 @@ class Gpx(Session):
 
         file = open('positions.csv', 'w')
 
+        file.close()
         return None
+
+    def get_all_pages(self, wd: webdriver) -> None:
+        compteur = 0
+        current_page = 'hihi'
+        _max = 10
+        while compteur < _max:
+            self.get_full_page(wd, current_page)
+
+
+    def get_full_page(self, wd: webdriver, current_page: str) -> None:
+        wd.get(current_page)
+        for i in range(1, 21):
+            _link = wd.find_element_by_xpath(f'//*[@id="search-results"]/tbody/tr[{i}]/td[3]/a')
+            _link.click()
+            time.sleep(1) # wait for load
+            dl_button = wd.find_element_by_xpath('/html/body/div[2]/div[3]/nav/div/div/ul/li[8]/a')
+            dl_button.click() #download the file
+            
+
+
+'''
+//*[@id="search-results"]/tbody/tr[1]/td[3]/a
+//*[@id="search-results"]/tbody/tr[7]/td[3]/a
+//*[@id="search-results"]/tbody/tr[8]/td[3]/a
+//*[@id="search-results"]/tbody/tr[20]/td[3]/a
+
+/html/body/div[2]/div[3]/nav/div/div/ul/li[8]/a
+'''
