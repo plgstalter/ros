b719c8e685927ee831a980532af1e8af6ff615a5
plgstalter <pierrelouisgstalter67@gmail.com>
2021-11-16 15:59:54 +0100

    affichage

--- src/app_project.xcworkspace/display.py
+++ src/app_project.xcworkspace/display.py
@@ -3,5 +3,12 @@ from gpx import Gpx
 import matplotlib.pyplot as plt
 import numpy as np
 
-positions = open('positions.csv', 'r')
-
+def show(save=False):
+    positions = open('positions.csv', 'r')
+    plt.figure();
+    for point in positions:
+        plt.scatter(point, c='blue');
+    if save:
+        plt.savefig('positions.png')
+    plt.show();
+    positions.close()
